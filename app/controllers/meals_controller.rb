0523b9e129f30cbce6ade46aa103b08e39678230
Allan McLelland <awm@foraker.com>
2015-11-19 23:12:21 -0700

    Add recipe for meals, allow meal editing

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -14,10 +14,19 @@ class MealsController < ApplicationController
 
   def create
   	@meal = Meal.create(meal_params)
-  	redirect_to meals_path(@meal)
+  	redirect_to meal_path(@meal)
+  end
+
+  def edit
+    @meal = Meal.find(params[:id])
+  end
+
+  def update
+    Meal.find(params[:id]).update_attributes(meal_params)
+    redirect_to meal_path(Meal.find(params[:id]))
   end
 
   def meal_params
-    params.require(:meal).permit(:name, :image)
+    params.require(:meal).permit(:name, :image, :recipe)
   end
 end
